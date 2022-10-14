1739e4dc399f92fad8b8598f39fa45af7299f00f
jimmy <vphu456@gmail.com>
2022-10-14 16:48:03 +0700

    fix bug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,19 +36,22 @@ export default function Navbar(props) {
         <div className="logo md:w-45 cursor-pointer" onClick={goHome}>
           <img src={loGo} alt="logo" className="w-full" />
         </div>
+        {/* search    */}
         <div
           className="navSearch pr-5 flex justify-end "
           style={{ color: "var(--color-white)" }}
         >
           <FiSearch className="iconSearch cursor-pointer w-5 h-5 translate-x-6 translate-y-3 hover:text-white text-gray-300	"></FiSearch>
+
           <input
             type="text"
             placeholder="Search"
-            className="text-base outline-none border-2 p-2.5 opacity-0	bg-zinc-800 cursor-pointer	
+            className="text-base outline-none border-2 p-2.5 w-0 opacity-0	bg-zinc-800 cursor-pointer	
             focus:pl-7 focus:w-80 focus:cursor-text	focus:opacity-100 focus:rounded-md"
             onChange={handleChangeInput}
           />
         </div>
+
         <ul className="navList flex ml-9 ">
           <li className="navItem">
             <NavLink to="/">Home</NavLink>
